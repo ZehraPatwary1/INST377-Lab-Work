74d947031e11c5118af5bef74e308a0b03f0c316
ZehraPatwary1 <98408063+ZehraPatwary1@users.noreply.github.com>
2022-02-17 17:05:21 -0500

    tetris

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         [1, width+1, width*2+1, 2],
         [width,width+1, width+2, width*2+2],
         [1, width+1, width*2+1, width*2],
-        [width, width*2, width*2+1]
+        [width, width*2, width*2+2]
     ]
     
 }) 
