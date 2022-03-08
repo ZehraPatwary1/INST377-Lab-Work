ce4351ff3846010a2b8b34b6dc3a08f3f4c6df10
ZehraPatwary1 <98408063+ZehraPatwary1@users.noreply.github.com>
2022-03-08 13:23:13 -0500

    added dataHandler

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,14 +1,28 @@
+function getRandomIntInclusive(min, max) {
+}
+
+function dataHandler(dataArray) {
+  console.log('fired dataHandler');
+  console.table(dataArray);
+  const range = [...Array(15).keys()];
+  range.forEach((item) => {
+    console.log('range item', item);
+  })
+} 
+
+// As the last step of your lab, hook this up to index.html
 async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form');
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
-  });
+  console.log('script loaded');
+  const form = document.querySelector('.main_form'); // change this selector to match the id or classname of your actual form
+  const submit = document.querySelector('.submit_button');
+  const results = await fetch('/api/foodServicesPG');
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  if (arrayFromJson.data.length > 0) {
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      console.log('form submission'); // this is substituting for a "breakpoint"
+    });
+  }
 }
 
 // this actually runs first! It's calling the function above
