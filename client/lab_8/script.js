28b8079956b996a527b09d87f0b6ef5123157e40
ZehraPatwary1 <98408063+ZehraPatwary1@users.noreply.github.com>
2022-04-06 13:02:32 -0400

    fixed load list

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -2,41 +2,38 @@ function getRandomIntInclusive(min, max) {
   const newMin = Math.ceil(min);
   const newMax = Math.floor(max);
   return Math.floor(
-    Math.random() * (newMax-newMin +1) + newMin
+    Math.random() * (newMax - newMin + 1) + newMin
   );
 }
 
 function restoArrayMake(dataArray) {
-  //console.log('fired dataHandler');
-  //console.table(dataArray);
+  // console.log('fired dataHandler');
+  // console.table(dataArray);
   const range = [...Array(15).keys()];
   const listItems = range.map((item, index) => {
     const restNum = getRandomIntInclusive(0, dataArray.length - 1);
 
     return dataArray[restNum];
   });
-    //console.log(listItems);
+  // console.log(listItems);
   return listItems;
-
 }
 function createHtmlList(collection) {
-  //console.log('fired HTML creator');
-  //console.log(collection);
-  const targetList = document.querySelector(".resto-list");
+  // console.log('fired HTML creator');
+  // console.log(collection);
+  const targetList = document.querySelector('.resto-list');
   targetList.innerHTML = '';
   collection.forEach((item) => {
     const {name} = item;
     const displayName = name.toLowerCase();
     const injectThisItem = `<li>${displayName}</li>`;
     targetList.innerHTML += injectThisItem;
-
   });
-
 }
 
 function initMap(targetId) {
-  const latLong = [38.7849, -76.8721];
-  const map = L.map(targetId).setView(latLong, 13);
+  // const latLong = [38.7849, 76.8721];
+  const map = L.map(targetId).setView([51.505, -0.09], 13);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -46,56 +43,35 @@ function initMap(targetId) {
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
   }).addTo(map);
   return map;
-
-}
-function addMapMarkers(map, collection){
-  collection.forEach(item => {
-    const point = item.geocoded_column_1?.coordinates;
-    console.log(item.geocoded_column_1?.coordinates);
-    L.marker(point[1], point[0]).addTo(map);
-  });
 }
 
-
-// As the last step of your lab, hook this up to index.html
-async function mainEvent() { // the async keyword means we can make API requests
+async function mainEvent() { 
   console.log('script loaded');
-  const form = document.querySelector('.main_form'); // change this selector to match the id or classname of your actual form
+  const form = document.querySelector('.main_form'); 
   const submit = document.querySelector('.submit_button');
 
   const resto = document.querySelector('#resto_name');
   const zipcode = document.querySelector('#zipcode');
   const map = initMap('map');
-  const retrievalVar = 'restaurants';
   submit.style.display = 'none';
 
-  if (localStorage.getItem(retrievalVar) === undefined) {
-    const results = await fetch('/api/foodServicesPG');
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.log(arrayFromJson);
-    localStorage.setItem(retrievalVar, JSON.stringify(arrayFromJson.data));
-  }
+  const results = await fetch('/api/foodServicesPG');
+  const arrayFromJson = await results.json(); 
+  console.log(arrayFromJson);
+  // const arrayFromJson = {data: []};
 
-  const storedDataString = localStorage.getItem(retrievalVar);
-
-  const storedDataArray = JSON.parse(storedDataString);
-
-  console.log(storedDataArray);
-  //const arrayFromJson = {data: []};
-
-  if (storedDataArray.length > 0) {
+  if (arrayFromJson.data.length > 0) {
     submit.style.display = 'block';
 
     let currentArray = [];
     resto.addEventListener('input', async(event) => {
       console.log(event.target.value);
 
-      //if (currentArray.length < 1) {
-        //console.log('empty');
-       // return;
-      //}
+      if (currentArray.length < 1) {
+        return;
+      }
 
-      const selectResto = storedDataArray.filter((item) => {
+      const selectResto = currentArray.filter((item) => {
         const lowerName = item.name.toLowerCase();
         const lowerValue = event.target.value.toLowerCase();
         return lowerName.includes(lowerValue);
@@ -105,18 +81,15 @@ async function mainEvent() { // the async keyword means we can make API requests
       createHtmlList(selectResto);
     });
 
+    form.addEventListener('submit', async (submitEvent) => { 
+      submitEvent.preventDefault(); 
+      // console.log('form submission'); // this is substituting for a "breakpoint"
 
-    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-      submitEvent.preventDefault(); // This prevents your page from refreshing!
-      //console.log('form submission'); // this is substituting for a "breakpoint"
-
-      currentArray = restoArrayMake(storedDataArray);
+      currentArray = restoArrayMake(arrayFromJson.data);
       console.log(currentArray);
       createHtmlList(currentArray);
-      addMapMarkers(map, currentArray);
     });
   }
 }
 
-// this actually runs first! It's calling the function above
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
